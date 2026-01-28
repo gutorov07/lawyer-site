17344674849da9b59a3f014ec46f65fb08f92e64
gutorov07 <mspelmen5@gmail.com>
2026-01-28 22:46:47 +0300

    iii

--- lawyer_website/settings.py
+++ lawyer_website/settings.py
@@ -2,7 +2,7 @@
 import os
 from pathlib import Path
 
-bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
+bind = f"0.0.0.0:{os.environ.get('PORT', '8080')}"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'lawyer_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PGDATABASE'),
+        'USER': os.environ.get('PGUSER'),
+        'PASSWORD': os.environ.get('PGPASSWORD'),
+        'HOST': os.environ.get('PGHOST'),
+        'PORT': os.environ.get('PGPORT', '5432'),
     }
 }
 
